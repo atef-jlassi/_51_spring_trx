95d3c98b834c3ad133d5b354a3a19089c4b719b6
Atef Jlassi <Atef.jlassi@value.com.tn>
2023-05-18 14:20:09 +0100

    add comments

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     try(var context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
 
       ProductService service = context.getBean(ProductService.class);
--- src/main/java/com/services/ProductService.java
+++ src/main/java/com/services/ProductService.java
@@ -11,9 +11,23 @@ public class ProductService {
 
   private final ProductRepository productRepository;
 
+  /**
+   * by default @Transactional rollback runtime exception but does not rollback checked exception
+   */
+
+  //  @Transactional(rollbackFor = Exception.class)
   @Transactional
-  public void addOneProduct() {
+  public void addOneProduct() throws Exception {
     productRepository.addProduct("Beer");
-    throw new RuntimeException(" :(");
+    try {
+      // in this case the rollback will not be executed because of the try catch bloc
+      // l'exception n'est pas propagé a l'exterieur, elle traité localement donc =
+      // le rollback ne se déclanche pas
+      throw new RuntimeException(" :(");
+    } catch (RuntimeException e) {
+      e.printStackTrace();
+    }
+//    throw new Exception(":(");
+//    throw new RuntimeException(" :(");
   }
 }
